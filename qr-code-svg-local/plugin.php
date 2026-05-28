dacccbadc62dda7bd4bfd3ab45c91da6afa61ae2
laithalsunni <lalsunn1@binghamton.edu>
2026-05-28 16:38:36 +0300

    Update plugin.php

--- qr-code-svg-local/plugin.php
+++ qr-code-svg-local/plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Branded QR Code Suite
 Plugin URI: https://github.com/laithalsunni/yourls-local-branded_qr-code
 Description: Locally generated, highly customizable, vector-perfect branded QR codes matching logo palettes dynamically.
-Version: 2.0
+Version: 2.1
 Author: Laith Alsunni
 Author URI: https://github.com/laithalsunni
 */
@@ -15,15 +15,12 @@ if( !defined( 'YOURLS_ABSPATH' ) ) die();
 yourls_add_action( 'html_head', 'branded_qr_inject_assets' );
 
 function branded_qr_inject_assets() {
-    // Dynamically calculate paths to keep assets completely decoupled and independent
     $plugin_url = yourls_plugin_url( dirname( __FILE__ ) );
-    
-    // Fall back safely to server root URL mapping rules if required
     $site_url = yourls_site_url();
     
     echo "\n\n";
     echo "<script type=\"text/javascript\">\n";
-    echo "  var BRANDED_QR_WEBROOT = '" . rtrim($site_url, '/') . "/qr/index.html';\n";
+    echo "  window.BRANDED_QR_WEBROOT = '" . rtrim($site_url, '/') . "/qr/index.html';\n";
     echo "</script>\n";
     echo "<script src=\"" . $plugin_url . "/inline-qrcode.js\" type=\"text/javascript\"></script>\n";
     echo "<style>div.branded-share-qr { float: right; margin-right: 0.2em !important; padding: 0 5px 10px !important; text-align: center; } div.branded-share-qr img { width: 100px; height: 100px; border: 1px solid #e1e4e6; border-radius: 4px; padding: 4px; background: #fff; transition: transform 0.2s; } div.branded-share-qr img:hover { transform: scale(1.05); }</style>\n";
